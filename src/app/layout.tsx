7556fed19c927591f1e881fa76bed147af395f01
Zasha <zahid.shaikh@live.com>
2025-11-02 17:54:10 +0530

    enbled vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Deck Battle - Epic Card Combat Game',
@@ -19,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+        <Analytics/>
       <body className="">
         {children}
       </body>
